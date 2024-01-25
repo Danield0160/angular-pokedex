b80a3e74c8d76098867c343bc1b7379a721a4931
Daniel windows <daniel.rosles@gmail.com>
2024-01-25 19:11:27 +0000

    v0.2

--- src/app/pokemon.service.ts
+++ src/app/pokemon.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 
 let api_all_pokemon = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1305";
-let someObj:Object;
 
 @Injectable({
   providedIn: 'root'
@@ -9,26 +8,30 @@ let someObj:Object;
 
 export class PokemonService {
 
-  private _listado_pokemon: string[] = [];
+  private listado_pokemon:any = [];
 
-  constructor() { }
-
-  get listado_pokemon(){
-    return[...this._listado_pokemon];
+  constructor(){
+    this.cargar()
   }
-
+  
   async cargar(){
-    let datos
-    await fetch(api_all_pokemon).then(response => response.json()).then(data => datos = data.results)
-
-    console.log(datos![0]["name"])
+    console.log("cargando datos")
+    const response = await fetch(api_all_pokemon);
+    const data = await response.json();
+    this.listado_pokemon = data.results;
   }
 
-  prueba(){
-    if (!this.listado_pokemon.length){
-      this.cargar()
-    }
+  obtener_todo(){
+    return this.listado_pokemon
   }
 
+  obtener_coincidencias_por_nombre(nombre:string){  }
+
+  obtener_sprite(id:Number){  }
+
+  obtener_descripcion(id:number){  }
+
+  obtener_habitat(id:number){  }
+
+  obtener_tipos(id:number){  }
 }
-
--- src/app/search-sidebar/search-sidebar.component.ts
+++ src/app/search-sidebar/search-sidebar.component.ts
@@ -1,19 +1,27 @@
 import { Component } from '@angular/core';
 import { PokemonService } from '../pokemon.service';
 
+let pokeservice = new PokemonService()
+
 @Component({
   selector: 'app-search-sidebar',
-  template: `
-    <h5>Buscar Pokémon:</h5>
-    <input type="text" class="form-control" placeholder="Buscar Pokémon..."
-      (keyup.enter)="searchTag()"
-      #txtTagInput>
-  `,
+  templateUrl: './search-sidebar.component.html',
+
   providers:[PokemonService]
 })
+
+
 export class SearchSidebarComponent {
   searchTag() {
-    console.log(new PokemonService().prueba());
+    let datos = pokeservice.obtener_todo()
+    console.log(datos);
   }
 
 }
+
+
+  //   <h5>Buscar Pokémon:</h5>
+  //   <input type="text" class="form-control" placeholder="Buscar Pokémon..."
+  //     (keyup.enter)="searchTag()"
+  //     #txtTagInput>
+  // `,
